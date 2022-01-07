e781166b0305af70c6c5fdd8093807cfd50541e4
1AndresBrown1 <aandresbrown11@gmail.com>
2022-01-07 17:22:57 -0500

    Pagina en desarollo ( No subir cambios al hosting )

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
         if(this.scrollY > 20){
-            $('.navbar_top').addClass("sticky");
+            $('.navbar-top').addClass("sticky");
         }else{
-            $('.navbar_top').removeClass("sticky");
+            $('.navbar-top').removeClass("sticky");
         }
-
+        
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
@@ -22,27 +22,27 @@ $(document).ready(function(){
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar_top .menu li a').click(function(){
+    $('.navbar-top .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
-        $('.navbar_top .menu').toggleClass("active");
+        $('.navbar-top .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Compromiso", "Responsabilidad", "Entusiasmo","Actitud","Respeto"],
+        strings: ["ES UN REGALO", "PARA SIEMPRE",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Noticias","Nuevos"],
+        strings: ["Compromiso", "Responsabilidad", "Entuciasmo", "Actitud", "Respeto"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -52,6 +52,7 @@ $(document).ready(function(){
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
+        autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
@@ -70,42 +71,3 @@ $(document).ready(function(){
         }
     });
 });
-
-
-// Galeria inicio
-[...document.querySelectorAll(".single-column")].map(column => {
-    column.style.setProperty("--animation", "slide");
-    column.style.setProperty("height", "200%");
-    column.innerHTML = column.innerHTML + column.innerHTML;
-  });
-  
-
-  let slider = document.querySelector(".slider-contenedor")
-  let sliderIndividual = document.querySelectorAll(".contenido-slider")
-  let contador = 1;
-  let width = sliderIndividual[0].clientWidth;
-  let intervalo = 3000;
-  
-  window.addEventListener("resize", function(){
-      width = sliderIndividual[0].clientWidth;
-  })
-  
-  setInterval(function(){
-      slides();
-  },intervalo);
-  
-  
-  
-  function slides(){
-      slider.style.transform = "translate("+(-width*contador)+"px)";
-      slider.style.transition = "transform .8s";
-      contador++;
-  
-      if(contador == sliderIndividual.length){
-          setTimeout(function(){
-              slider.style.transform = "translate(0px)";
-              slider.style.transition = "transform 0s";
-              contador=1;
-          },1500)
-      }
-  }
