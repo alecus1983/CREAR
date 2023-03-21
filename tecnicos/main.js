14c88744f3353a3cc2854beec04131a64b5b4bc6
Andres Brown <77754732+1AndresBrown1@users.noreply.github.com>
2023-03-21 08:55:22 -0500

    Archivos duplicados o que img que no se utilizan

--- tecnicos/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('#temario .lista a').forEach((elemento) => {
-	elemento.addEventListener('click', () => {
-		document.querySelector('#temario .activo').classList.remove('activo');
-		elemento.parentElement.classList.add('activo');
-	});
-});
