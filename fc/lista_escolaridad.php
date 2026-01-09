7e522e0b56260a91b4dd631c29ac13d31a04e09b
alecus1983 <34242304+alecus1983@users.noreply.github.com>
2026-01-09 02:12:37 -0500

    cambios

--- fc/lista_escolaridad.php
+++ fc/lista_escolaridad.php
@@ -1,25 +1,34 @@
 <?php
-// archivo que obtiene los docentes matriculados
-// en una materia para un grado/curso/jornada
+// archivo que obtiene en formato json
+// la lista de escolaridades de la institucion
 
 require_once "datos.php";
 
 //variables de validacion
 $valido = true;
 $err = "";
+$year = date('Y');
+
+
+// recibo codigo del docente
+$id_docente = $_POST["id_docente"];
 //array de respuestals
 $respuesta = [];
 
-
-
-// creo un nuevo objeto jornada
+// creo un nuevo objeto escolaridad vacio
 $e = new escolaridad();
-// selecciono una lista de jornadas
+
+// selecciono un listado de escolaridades
 $lista = $e->lista();
 
+// si obtengo un listado entonces
+ if (isset($lista)) {
 
-if (isset($lista)) {
-    // encapsulo  la respuesta en modo json
-    echo json_encode($lista);
-}
+    //$respuesta['status'] = 1;
+    //$respuesta['html'] = $lista;
+     // encapsulo  la respuesta en modo json
+     echo json_encode($lista);
+ }
+
+
 ?>
