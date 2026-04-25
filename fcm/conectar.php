a3c0af4e75e694d2d5e08b68958ca51f8e3d9b48
alecus1983 <34242304+alecus1983@users.noreply.github.com>
2026-04-24 21:20:23 -0500

    cambios de directorio

---
+++ fcm/conectar.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "imcreati_admin");
+define("DB_PASS", "conezioncrear21");
+define("DB_NAME", "imcreati_datam");
+define("DB_CHARSET", "utf8mb4");
+?>
