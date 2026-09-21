722310d6f9f8e784331c2d25c78de8c9646c2a6a
alecus1983 <alejandropalacioscarabali@gmail.com>
2026-09-21 12:13:56 -0500

    ajuste recuperacion

---
+++ fcm/js/recuperacion.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+
+    // funcion que muestra el listado de recuperaciones
+
+    function load_lista_recuperacion() {
+
+        $.ajax({
+            type: 'POST',
+            url: 'listado_estudiantes_recupeacion.php',
+            data: {
+                years: $('#years').val(),
+                id_g: $('#id_g').val(),
+                id_ms: $('#id_ms').val(),
+                id_jornada: $('#jornada').val(),
+                periodo: $('#periodos').val(),
+                curso: $('#id_c').val(),
+                semana: $('#semana').val()
+            },
+            beforeSend: function () {
+                $('#loader').show();
+            },
+            success: function (respuesta) {
+                $('#calificador').html(respuesta);
+                $('#loader').hide();
+                swal('Éxito', 'Lista de estudiantes cargada correctamente.', 'success');
+            },
+            error: function (xhr, status) {
+                $('#loader').hide();
+                swal('Error', 'Disculpe, existió un problema al cargar los estudiantes.', 'error');
+                console.log(xhr);
+            }
+        });
+
+    }
+});
